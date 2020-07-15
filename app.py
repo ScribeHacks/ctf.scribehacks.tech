a9f1c869cc1f0dba08ced3b64229a357e4d87ff6
Kai Devrim <kkgithub@devrim.tech>
2020-07-14 18:18:02 -0700

    Starting work on the login system

--- app.py
+++ app.py
@@ -1,28 +1,41 @@
-from flask import Flask, request, render_template
-from answers import *
+from flask import Flask, request, render_template, redirect, url_for
 import os
 from dotenv import load_dotenv
 load_dotenv()
 # from datetime import datetime
 
 app = Flask(__name__)
+loggedIn = False
 
-@app.route('/')
-def my_form():
-    return render_template("index.html")
 
-@app.route('/', methods=['POST'])
-def response():
-    flag = request.form["flag"]
-    processed_text = flag.upper()
-    first_flag = os.getenv("FLAG_1")
-    print(first_flag)
-    if (processed_text == first_flag):
-        correct_answer = "Your flag is correct!"
-    else:
-        correct_answer = "Your flag is incorrect. Please try again."
+if (loggedIn == True):
+    @app.route('/ctf', methods=['POST'])
+    def flags():
+        flag = request.form["flag"]
+        processed_text = flag.upper()
+        first_flag = os.getenv("FLAG_1")
+        print(first_flag)
+        if (processed_text == first_flag):
+            correct_answer = "Your flag is correct!"
+        else:
+            correct_answer = "Your flag is incorrect. Please try again."
+        return render_template("index.html", is_correct = correct_answer, flag=flag)
+else:
+    @app.route('/', methods=['GET', 'POST'])
+    def login():
+        error = None
+        if request.method == 'POST':
+            if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+                error = 'Invalid Credentials. Please try again.'
+            else:
+                loggedIn = True
+                return render_template('index.html')
+        return render_template('login.html', error=error)
 
-    return render_template("index.html", is_correct = correct_answer, flag=flag)
+
+
+
+
  # Launch the FlaskPy dev server
 app.run(host="localhost", debug=True)
 
