103a8f0b754757513339d87ef8983f865e9abd69
Kai Devrim <kkgithub@devrim.tech>
2020-07-22 19:07:02 -0700

    dumb flask-login thing

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, render_template, redirect, url_for, Blueprint
 import os
 from dotenv import load_dotenv
-import init_db
 import sqlite3
-load_dotenv()
+
 # from datetime import datetime
 
 app = Flask(__name__)
+load_dotenv()
 
 connection = sqlite3.connect('database.db')
+db = SQLAlchemy()
 
 
 loggedIn = False
@@ -18,56 +19,12 @@ username = ""
 userteam = ""
 
 
-@app.route('/ctf', methods=['GET', 'POST'])
-def flags():
-    global loggedIn
-    while loggedIn == True:
-        global username
-        global userteam
-        # global loggedIn
-        if request.form['btn_logout'] == 'logout':
-            # global loggedIn
-            loggedIn = False
-        return render_template("index.html", username=username, userteam=userteam)
-    else:
-        return redirect('/')
+@main.route('/')
+def index():
+    return render_template('index.html')
 
 
-@app.route('/', methods=['GET', 'POST'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] == '' or request.form['userteam'] == '':
-            error = 'Did not provide either username or team'
-        else:
-            global currentID
-            currentID += 1
-            global username
-            global userteam
-            username = request.form['username']
-            userteam = request.form['userteam']
-
-            with sqlite3.connect('database.db') as con:
-                cursor = con.cursor()
-
-                if cursor.execute("SELECT UserCount FROM Teams WHERE TeamName = ?", (userteam,)) == 4:
-                    error = "Team is full"
-                else:
-                    cursor.execute(
-                        "UPDATE Teams SET UserCount = UserCount + 1 WHERE TeamName = ?", (userteam,))
-                    cursor.execute(
-                        "INSERT INTO Users (Username, Team) VALUES (?, ?)", (username, userteam))
-                    error = ''
-                con.commit()
-
-            global loggedIn
-            if error == '':
-                loggedIn = True
-                return redirect('/ctf')
-    return render_template('login.html', error=error)
-
-
-    # Launch the FlaskPy dev server
+# Launch the FlaskPy dev server
 app.run(host="localhost", debug=True)
 
 # def hello():
@@ -94,3 +51,47 @@ app.run(host="localhost", debug=True)
 # return render_template("index.html", is_correct=correct_answer, flag=flag)
 
 # if loggedIn == True:
+
+
+# @app.route('/ctf', methods=['GET', 'POST'])
+# def flags():
+#     global loggedIn
+#     while loggedIn == True:
+#         global username
+#         global userteam
+#         # global loggedIn
+#         if request.form['btn_logout'] == 'logout':
+#             # global loggedIn
+#             loggedIn = False
+#         return render_template("index.html", username=username, userteam=userteam)
+#     else:
+#         return redirect('/')
+# @app.route('/', methods=['GET', 'POST'])
+# def login():
+#     error = None
+#     if request.method == 'POST':
+#         if request.form['username'] == '' or request.form['userteam'] == '':
+#             error = 'Did not provide either username or team'
+#         else:
+#             global currentID
+#             currentID += 1
+#             global username
+#             global userteam
+#             username = request.form['username']
+#             userteam = request.form['userteam']
+#             with sqlite3.connect('database.db') as con:
+#                 cursor = con.cursor()
+#                 if cursor.execute("SELECT UserCount FROM Teams WHERE TeamName = ?", (userteam,)) == 4:
+#                     error = "Team is full"
+#                 else:
+#                     cursor.execute(
+#                         "UPDATE Teams SET UserCount = UserCount + 1 WHERE TeamName = ?", (userteam,))
+#                     cursor.execute(
+#                         "INSERT INTO Users (Username, Team) VALUES (?, ?)", (username, userteam))
+#                     error = ''
+#                 con.commit()
+#             global loggedIn
+#             if error == '':
+#                 loggedIn = True
+#                 return redirect('/ctf')
+#     return render_template('login.html', error=error)
--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-load_dotenv()
-
-first_flag = os.getenv("FLAG_1")
-print(first_flag)
